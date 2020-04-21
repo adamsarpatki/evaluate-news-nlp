6676ff2f2e88f4817f5aa2cf6110efca202048c1
Sárpátki Ádám <sepa1989@gmail.com>
2020-04-21 17:23:00 +0200

    feat: install aylien, dotenv and jest

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,16 @@
+var aylien = require("aylien_textapi");
+const dotenv = require('dotenv');
+dotenv.config();
+
+// set aylien API credentials
+// NOTICE that textapi is the name I used, but it is arbitrary.
+// You could call it aylienapi, nlp, or anything else, 
+//   just make sure to make that change universally!
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
