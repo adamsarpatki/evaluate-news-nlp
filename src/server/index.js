5d480e63908ad0c8445a2fa98fe54f83419dccd0
Sárpátki Ádám <sepa1989@gmail.com>
2020-04-21 18:46:35 +0200

    feat: set up output for webpack.dev

--- src/server/index.js
+++ src/server/index.js
@@ -27,10 +27,16 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+/* app.get('/test', function (req, res) {
+    textapi.sentiment({
+        'text': 'John is a very good football player!'
+      }, function(error, response) {
+        if (error === null) {
+          console.log(response);
+        }
+      });
+}) */
